eaf2766620aa0357cbb3588e97b6a3f2526c03f5
Dare <joelliju10@gmail.com>
2021-12-13 14:36:11 -0500

    Now it kinda does forward pass

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,14 +1,15 @@
 import random
+import math
 def activation(value):
-    return 0
+    return 1/(1+math.pow(math.e,-value))
 """
 this is the node class
 """
 class Node:
     def __init__(self):
+        self.input = 0
+        self.output = 0
         pass
-    def output(self,input):
-        self.output = input
     def addinput(self, input):
         self.input = self.input + input
     """
@@ -16,7 +17,9 @@ class Node:
     """
     def passforward(self):
         pass
-    def output(self):
+    def setOutput(self,output):
+        self.output = output
+    def Output(self):
         try:
             self.output  = activation(self.input)
         except:
@@ -33,16 +36,29 @@ class Layer:
         self.nodes = []
         for i in range(layersize):
             self.nodes.append(Node())
+    """
+    takes the activation function and sets the output 
+    """
+    def calculateOuput(self):
+        for node in self.nodes:
+            node.output = activation(node.input)
+    def setOutput(self,output):
+        for i in range(len(self.nodes)):
+            self.nodes[i].output = output[i]
+
+    def setInput(self, Input):
+        for i in range(len(self.nodes)):
+            self.nodes[i].input = Input[i]
 
 """
 this is the NeuralNet
 """
 class NeuralNet:
     def __init__(self, layersizes):
+        random.seed(100)
         self.layers = []
         self.weights = []
         for i in range(len(layersizes)):#per layers
-            
             self.layers.append(Layer(layersizes[i]))
             layerweight = []
             if i+1 < len(layersizes):
@@ -54,3 +70,19 @@ class NeuralNet:
                         eachLayer.append(t)
                     layerweight.append(eachLayer)
                 self.weights.append(layerweight)
+    """
+    this function takes the input given, and forward passes the information.
+    """
+    def forwardpass(self,values):
+            self.layers[0].setInput(values)# first layer
+            self.layers[0].setOutput(values)  # first layer
+            for i in range(len(self.layers)):#each layer
+                try:
+                    for node in range(len(self.layers[i+1].nodes)):#for each node in next layer
+                        for n in range(len(self.layers[i].nodes)):#for each node in current layer
+                            self.layers[i+1].nodes[node].addinput(self.layers[i].nodes[n].output*self.weights[i][n][node])
+                    self.layers[i+1].calculateOuput()
+                except:
+                    #output layer
+                    pass
+            #self.layers[len(self.layers)-1].setOutput(output)#setting the outputs for training.
--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 import NeuralNetwork
 
 C = NeuralNetwork.NeuralNet([3,2,1])
+C.forwardpass([2,3,1])
 print(C.weights)
+for layer in C.layers:
+    for node in layer.nodes:
+        print(node.input)
+        print(node.output)
+        print()
